9aedfd9162d8c345cecefab5685f05f4d223106b
Felicitas Kuhn <hkuhn@stud.hs-bremen.de>
2021-05-27 15:42:58 +0200

    Erster Eintrag

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author kuhnh
+ */
+import java.util.Scanner;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
